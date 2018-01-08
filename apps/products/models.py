405ee307a1c79dc5330f361cb2cbe132bdd1976a
juan <elbarto2589@gmail.com>
2018-01-08 16:30:10 -0500

    clear

--- apps/products/models.py
+++ apps/products/models.py
@@ -5,10 +5,48 @@ class Brand(models.Model):
     name = models.CharField(max_length=120)
 
 
-class ShopDepartment(models.Models):
+class ShopDepartment(models.Model):
     name = models.CharField(max_length=50)
 
 
-class Category(models.Models):
+class Category(models.Model):
     name = models.CharField(max_length=50)
-    team = models.ForeignKey(ShopDepartment)
+    category = models.ForeignKey(
+        ShopDepartment,
+        related_name='categories',
+        on_delete=models.DO_NOTHING,
+    )
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=50)
+    discounts = models.IntegerField()
+    description = models.CharField(max_length=150)
+    brand = models.ForeignKey(
+        Brand,
+        related_name='products',
+        on_delete=models.DO_NOTHING,
+    )
+    category = models.ForeignKey(
+        Category,
+        related_name='products',
+        on_delete=models.DO_NOTHING,
+    )
+
+
+class ImageProduct(models.Model):
+    url = models.URLField()
+    product = models.ForeignKey(
+        Product,
+        related_name='image_product',
+        on_delete=models.DO_NOTHING,
+    )
+
+
+class Publicity(models.Model):
+    url = models.URLField()
+    product = models.ForeignKey(
+        Product,
+        related_name='publicity',
+        on_delete=models.DO_NOTHING,
+    )
