435d28aa5960b6fe773a1ed7f7a57b007fbc1bb1
juan <elbarto2589@gmail.com>
2018-01-17 17:44:10 -0500

    config admin

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from .models import ShopDepartment
+from .models import ShopDepartment, Category
+
 
 @admin.register(ShopDepartment)
 class ShopDepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
     pass
