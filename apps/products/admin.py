cadc4d97c5f955bc0be62cdfdfa04f0b787028f1
juan <elbarto2589@gmail.com>
2018-01-20 17:26:01 -0500

    admin brand

--- apps/products/admin.py
+++ apps/products/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import ShopDepartment, Category
+from .models import ShopDepartment, Category, Brand
 
 
 @admin.register(ShopDepartment)
 class ShopDepartmentAdmin(admin.ModelAdmin):
     list_display = ['name', 'active']
     list_editable = ['active']
+
+
+@admin.register(Brand)
+class BrandAdmin(admin.ModelAdmin):
+    list_per_page = 20
 
 
 @admin.register(Category)
