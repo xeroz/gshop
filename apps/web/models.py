09f87bb5e1efcb3f005cb4bf71f793cad534b9ba
juan <chavezvasquezjuan@gmail.com>
2018-02-03 12:19:51 -0500

    infosite

--- apps/web/models.py
+++ apps/web/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class InfoSite(models.Model):
+    facebook = models.URLField()
+    twitter = models.URLField()
+    instagram = models.URLField()
+    email = models.EmailField()
+
+
+
+class BannerHome(models.Model):
+    image = models.URLField()
