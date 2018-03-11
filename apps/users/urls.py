5d09cf1dcffd8ee3faec45b67d5f841448c88d4d
juan <chavezvasquezjuan@gmail.com>
2018-03-10 21:20:28 -0500

    app users

---
+++ apps/users/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+# from .views import index, ProductTemplateView, ProductDetailView
+
+app_name = 'products'
+
+urlpatterns = [
+    # path('index/', index, name='index'),
+    # path('categories/<slug>/', ProductTemplateView.as_view(), name='list'),
+    # path('product/<slug>/', ProductDetailView.as_view(), name='detail'),
+]
