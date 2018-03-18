f16a195114055cc1a53c3ba790cebb55e2b91c2b
juan <chavezvasquezjuan@gmail.com>
2018-03-17 21:39:52 -0500

    migrations users

--- apps/users/models.py
+++ apps/users/models.py
@@ -1,3 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    description = models.CharField(max_length=100, default='')
+    city = models.CharField(max_length=100, default='')
+    website = models.URLField(max_length=100, default='')
+    phone = models.IntegerField(default=0)
